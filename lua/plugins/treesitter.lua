1d4856f171299c6cdb26c340767aa0cbc80d35e0
hassanuahmad <ahmad12p@uwindsor.ca>
2024-01-03 18:37:26 -0500

    first commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"windwp/nvim-ts-autotag",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				autotag = { enable = true },
+			})
+		end,
+	},
+}
